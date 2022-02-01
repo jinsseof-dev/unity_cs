6ad3785d893905e4e47e01117059af94a2e3f702
jinsseof <jinsseof@jiran.com>
2022-02-01 20:54:41 +0900

    패스트캠퍼스 08일차 공부 내용

---
+++ FastCampus_Sample_CS/043_Operator_Branch2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _043_Operator_Branch2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("정수를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+            
+            if (num > 0)
+            {
+                Console.WriteLine("입력한 수는 양수!!");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("입력한 수는 음수!!");
+            }
+            else
+            {
+                Console.WriteLine("입력한 수는 0!!");
+            }
+        }
+    }
+}
---
+++ FastCampus_Sample_CS/043_Operator_branch3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _043_Operator_branch3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isA = false;
+            bool isB = true;
+            bool isC = true;
+            bool isD = false;
+
+            if(isB && isC)
+            {
+                Console.WriteLine("true && true => {0}", (isB && isC));
+            }
+
+            if(isC || isD)
+            {
+                Console.WriteLine("true || false => {0}", (isC || isD));
+            }
+
+            if (2 > 1 && 3 < 4 || 1 > 2) // 복합적으로 사용된 경우에는 앞에서부터 차례대로
+            {
+                Console.WriteLine("true && true || false => {0}", (2 > 1 && 3 < 4 || 1 > 2));
+            }
+        }
+    }
+}
