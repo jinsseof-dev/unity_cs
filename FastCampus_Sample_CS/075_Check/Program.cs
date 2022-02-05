fb90574b3963c65289dabf58d88cafd46ae3f7ba
jinsseof <jinsseof@jiran.com>
2022-02-05 23:57:52 +0900

    패스트캠퍼스 50일 챌린지 13일차

---
+++ FastCampus_Sample_CS/075_Check/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _075_Check
+{
+    internal class Program
+    {
+        static void InputID(int[] ID, int index)
+        {
+            Console.Write("학생 ID를 입력하세요: ");
+            ID[index] = int.Parse(Console.ReadLine());
+        }
+
+        static void InputKor(int[] kor, int index)
+        {
+            Console.Write("국어 점수를 입력하세요: ");
+            kor[index] = int.Parse(Console.ReadLine());
+        }
+
+        static void InputMath(int[] math, int index)
+        {
+            Console.Write("수학 점수를 입력하세요: ");
+            math[index] = int.Parse(Console.ReadLine());
+        }
+
+        static void InputEng(int[] eng, int index)
+        {
+            Console.Write("영어 점수를 입력하세요: ");
+            eng[index] = int.Parse(Console.ReadLine());
+        }
+
+        static void PrintID(int max, int[] ID)
+        {
+            for (int i = 0; i < max; i++)
+            {
+                Console.WriteLine("학생 ID: {0}", ID[i]);
+            }
+        }
+
+        static int CheckID(int id, int max, int[] ID)
+        {
+            for (int i = 0; i < max; i++)
+            {
+                if (ID[i] == id)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int id = 0;
+            int max = 3;
+            int[] kor = new int[3];
+            int[] math = new int[3];
+            int[] eng = new int[3];
+            int[] ID = new int[3];
+            int index = 0;
+
+
+            for (int i=0;i<max;i++)
+            {
+                InputID(ID, i);
+                InputKor(kor, i);
+                InputMath(math, i);
+                InputEng(eng, i);
+            }
+
+
+            while (true)
+            {
+                PrintID(max, ID);
+                Console.Write("학생 ID를 입력하세요(0 - 나가기): ");
+                id = int.Parse(Console.ReadLine());
+                index = CheckID(id, max, ID);
+
+                if (id == 0) {
+                    break;
+                }
+                else if (index == -1)
+                {
+                    Console.WriteLine("학생의 ID가 없어요. 다시 입력하세요");
+                }
+                else
+                {
+                    Console.WriteLine("국어 점수: {0}", kor[index]);
+                    Console.WriteLine("수학 점수: {0}", math[index]);
+                    Console.WriteLine("영어 점수: {0}", eng[index]);
+
+                    int total = (kor[index] + math[index] + eng[index]);
+                    Console.WriteLine("총점: {0}", total);
+                    Console.WriteLine("평균: {0}", (float)(total / 3));
+                }
+            }
+        }
+    }
+}
---
+++ FastCampus_Sample_CS/076_Check/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _076_Check
+{
+    internal class Program
+    {
+        static int InputNumber(int count)
+        {
+            if (count == 0)
+            {
+                Console.Write("첫번째 수를 입력해 주세요: ");
+                return int.Parse(Console.ReadLine());
+            }
+            else if (count == 1)
+            {
+                Console.Write("두번째 수를 입력해 주세요: ");
+                return int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void PrintResult(int a, int b)
+        {
+            Console.WriteLine("{0} + {1} = {2}", a, b, (a + b));
+        }
+        static bool CheckEnd()
+        {
+            while (true)
+            {
+                Console.Write("추가로 계산할까요?(1: OK, 0: NO, 단 총 10번까지 가능)");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    return true;
+                }
+                else if (input == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("잘못 입력하셨습니다.");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            const int COUNT = 10;
+            int count = 0;
+            int[,] number = new int[COUNT, 2];
+
+            for (int i = 0; i < COUNT; i++)
+            {
+                count++;
+                number[i, 0] = InputNumber(0);
+                number[i, 1] = InputNumber(1);
+                PrintResult(number[i, 0], number[i, 1]);
+
+                if (i == 9 || !CheckEnd())
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                PrintResult(number[i, 0], number[i, 1]);
+            }
+        }
+    }
+}
