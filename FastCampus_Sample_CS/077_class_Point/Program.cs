f35d9000fcf5215ec979e421538743a2b18d9d47
jinsseof <jinsseof@jiran.com>
2022-02-08 00:53:09 +0900

    패스트캠퍼스 50일 챌린지 15일차 - class

---
+++ FastCampus_Sample_CS/077_class_Point/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _077_class_Point
+{
+    public class Point
+    {
+        private int a;
+        public int b;
+        protected int c;
+
+        public int getA()
+        {
+            return a;
+        }
+        public bool setA(int _a)
+        {
+            this.a = _a;
+            return true;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 200;
+            bool result = false;
+            Point point = new Point();
+            result = point.setA(a);
+            point.b = b;
+            Console.WriteLine("a is {0}", point.getA());
+        }
+    }
+}
---
+++ FastCampus_Sample_CS/078_Constructor_Point/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _078_Constructor_Point
+{
+    public class Point
+    {
+        private int a;
+
+        public Point(int _a)
+        {
+            a = _a;
+            Console.WriteLine("Constructor is Called!!");
+            Console.WriteLine("attribute a is {0}", a);
+        }
+        ~Point()
+        {
+            a = 0;
+            Console.WriteLine("Destructor is Called!!");
+            Console.WriteLine("attribute a is {0}", a);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 101;
+            Point point = new Point(a);
+
+        }
+    }
+}
