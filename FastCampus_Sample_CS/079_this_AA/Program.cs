5236135fb7924deabef6a07ecb6f7ce4d8ae24fb
jinsseof <jinsseof@jiran.com>
2022-02-09 21:31:19 +0900

    패스트캠퍼스 50일 챌린지 16일차

---
+++ FastCampus_Sample_CS/079_this_AA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _079_this_AA
+{
+    class AA
+    {
+        private int a;
+        public AA(int a)
+        {
+            this.a = a;
+        }
+        public void Print()
+        {
+            int a = 100;
+            this.a = 1000;
+            Console.WriteLine("a is {0}", a);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA(10);
+            aa.Print();
+        }
+    }
+}
---
+++ FastCampus_Sample_CS/080_static/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _080_static
+{
+    internal class Program
+    {
+        class AA
+        {
+            public static int a;
+            public static int b;
+            public static readonly int c = 200; // readonly는 읽기 전용 속성을 부여
+
+            public static void Print()
+            {
+                Console.WriteLine("a is {0}", a);
+                Console.WriteLine("b is {0}", b);
+            }
+        }
+
+        class BB
+        {
+            public int c;
+            public int d;
+
+            public void Print()
+            {
+                Console.WriteLine("c is {0}", c);
+                Console.WriteLine("d is {0}", d);
+            }
+        }
+        static void Main(string[] args)
+        {
+            AA.a = 10;
+            AA.b = 100;
+            AA.Print();
+
+            BB bb = new BB();
+            bb.c = 10;
+            bb.d = 100;
+            bb.Print();
+
+            //BB.c = 10;
+            //BB.b = 100;
+            //BB.Print();
+        }
+    }
+}
