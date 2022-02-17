b3845f9f269c1f37b69385855aea966aad66f4fc
jinsseof <jinsseof@jiran.com>
2022-02-17 22:57:28 +0900

    패스트캠퍼스 50일 챌린지 24일차

---
+++ FastCampus_Sample_CS/097_Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _097_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+
+            for (int i = 0; i < 10; i++)
+            {
+                stack.Push(i);
+            }
+
+            Console.WriteLine("stack data Peek: {0}", stack.Peek());
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine("stack data: {0}, count: {1}", stack.Pop(), stack.Count);
+            }
+
+            // 배열 데이터로 초기화
+            Console.WriteLine("배열 데이터로 초기화");
+            int[] arrData = { 100, 200, 300 };
+            Stack stackCopy = new Stack(arrData);
+            
+            foreach(object data in stackCopy)
+            {
+                Console.WriteLine("stackCopy data: {0}", data);
+            }
+        }
+    }
+}
---
+++ FastCampus_Sample_CS/099_Indexor01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _099_Indexor01
+{
+    class AA
+    {
+        private int[] num = new int[10];
+
+        public int this[int index]
+        {
+            get { return num[index]; }
+            set { num[index] = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+
+            for (int i= 0; i < 10; i++)
+            {
+                aa[i] = i;
+            }
+
+            aa[0] = 10;
+            aa[1] = 200;
+            for (int i=0; i < 10; i++)
+            {
+                Console.WriteLine("aa[{0}]: {1}", i, aa[i]);
+            }
+        }
+    }
+}
