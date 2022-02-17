c692e3baf6ad8eb662561f86c332b8ef21944411
jinsseof <jinsseof@jiran.com>
2022-02-18 00:08:56 +0900

    패스트캠퍼스 50일 챌린지 25일

---
+++ FastCampus_Sample_CS/104_GenericClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _104_GenericClass
+{
+    class GenericAA<T>
+    {
+        private T num;
+
+        public T GetNum()
+        {
+            return num;
+        }
+        public void SetNum(T data)
+        {
+            num = data;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericAA<int> AA = new GenericAA<int>();
+            AA.SetNum(100);
+            Console.WriteLine("AA: {0}", AA.GetNum());
+
+            GenericAA<float> BB = new GenericAA<float>();
+            BB.SetNum(20.354f);
+            Console.WriteLine("BB: {0}", BB.GetNum());
+
+            GenericAA<string> CC = new GenericAA<string>();
+            CC.SetNum("Hello Generic");
+            Console.WriteLine("CC: {0}", CC.GetNum());
+        }
+    }
+}
