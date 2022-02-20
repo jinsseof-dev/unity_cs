859b56262d11c86bd1e128408c6b5ab4332755ef
jinsseof <jinsseof@jiran.com>
2022-02-20 16:04:40 +0900

    패스트캠퍼스 50일 챌린지 27일차

---
+++ FastCampus_Sample_CS/111_try_catch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _111_try_catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = 0;
+            bool isCorrect = false;
+
+            while(!isCorrect)
+            {
+                string readStr = Console.ReadLine();
+                try
+                {
+                    inputNum = int.Parse(readStr);
+                    isCorrect = true;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("입력문자: {0} - 정수를 입력하세요", readStr);
+                }
+            }
+            Console.WriteLine("inputNum: {0}", inputNum);
+        }
+    }
+}
---
+++ FastCampus_Sample_CS/112_Exception_Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _112_Exception_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNum = 10;
+
+            try
+            {
+                checked
+                {
+                    maxNum += Int32.MaxValue;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("e.Message: {0}", e.Message);
+                Console.WriteLine("e.Source: {0}", e.Source);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("e.Message: {0}", e.Message);
+            }
+
+            Console.WriteLine("maxNum: {0}", maxNum);
+
+        }
+    }
+}
