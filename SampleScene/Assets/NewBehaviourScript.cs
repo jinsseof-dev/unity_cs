f6c7dcde4cc1fc40a4698bf2ee85ca71d77f6ff8
jinsseof <jinsseof@jiran.com>
2022-03-15 23:13:17 +0900

    패스트캠퍼스 50일 챌린지 51일차

---
+++ SampleScene/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;         // 회색으로 표시되는 using 구문은
+using System.Collections.Generic; // 선언은 되었으나 사용된 곳이 없다는 의미
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
