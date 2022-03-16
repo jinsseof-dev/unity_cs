a167bb0c53872e1323578ec4062dadfc0c8d919b
jinsseof <jinsseof@jiran.com>
2022-03-16 23:32:54 +0900

    패스트캠퍼스 50일 챌린지 53일차 SerializeField scope

--- SampleScene/Assets/NewBehaviourScript.cs
+++ SampleScene/Assets/NewBehaviourScript.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField]
+    int myValue;
+    [SerializeField]
+    int myValue1;
+    [SerializeField]
+    int myValue2;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start() Log");
+        Debug.Log("Start() Log - myValue =" + myValue);
     }
 
     // Update is called once per frame
