a004f61c686cce8a620aad3b4736f27c22719c90
jinsseof <jinsseof@gmail.com>
2022-03-22 02:13:35 +0900

    패스트캠퍼스 50일 챌린지 57일차

---
+++ FirstWinger/Assets/StarSparrow/Readme/SystemManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+    static SystemManager instance = null;
+
+    public SystemManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("SystemManager error! Singletone error!");
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
