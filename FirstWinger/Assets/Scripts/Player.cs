cb338816cd3bda972e9c62bd9863c1d3d1e1b0c3
jinsseof <jinsseof@gmail.com>
2022-03-24 02:36:19 +0900

    패스트캠퍼스 50일 챌린지 59일차 오류 보정

--- FirstWinger/Assets/Scripts/Player.cs
+++ FirstWinger/Assets/Scripts/Player.cs
@@ -52,19 +52,19 @@ public class Player : MonoBehaviour
 
         result = boxCollider.transform.position + boxCollider.center + moveVector;
 
-        if (result.x - boxCollider.size.x * 0.5f < -MainBGQuadTransform.localScale.x * 0.5f)
+        if (result.x - boxCollider.size.x * 0.5f - 15 < -MainBGQuadTransform.localScale.x * 0.5f)
         {
             moveVector.x = 0;
         }
-        if (result.x + boxCollider.size.x * 0.5f > MainBGQuadTransform.localScale.x * 0.5f)
+        if (result.x + boxCollider.size.x * 0.5f - 15 > MainBGQuadTransform.localScale.x * 0.5f)
         {
             moveVector.x = 0;
         }
-        if (result.y - boxCollider.size.y * 0.5f < -MainBGQuadTransform.localScale.y * 0.5f)
+        if (result.y - boxCollider.size.y * 0.5f + 10.5f < -MainBGQuadTransform.localScale.y * 0.5f)
         {
             moveVector.y = 0;
         }
-        if (result.y + boxCollider.size.y * 0.5f > MainBGQuadTransform.localScale.y * 0.5f)
+        if (result.y + boxCollider.size.y * 0.5f + 10.5f > MainBGQuadTransform.localScale.y * 0.5f)
         {
             moveVector.y = 0;
         }
